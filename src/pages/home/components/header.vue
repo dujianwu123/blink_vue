3ba86f7a3aa993701ed376e817433a9022b6646d
dujianwu123 <dujianwu123@126.com>
2019-11-08 09:02:38 +0800

    'init'

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <span>头部</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .header{
+    width: 375px;
+    height: 100px;
+    border: 1px solid red;
+    span{
+      color: red;
+    }
+  }
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    首页
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/header'
+export default {
+  components: {
+    Header
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
